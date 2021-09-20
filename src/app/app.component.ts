e22b1bc6ddd9011ad2a20ad10602d493aebbae8e
Sonam020! <sonam.pushideasinc@gmail.com>
2021-09-20 18:46:01 +0530

    completed to add todo

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'to-do-list';
+public allItems:any = [];
+  
+  /* A two-way binding performed which
+     pushes text on division */
+public newTask:any;
+
+  /* When input is empty, it will
+     not create a new division */
+    //  addItem(value:any) {
+    //   this.allItems.push(value);
+    // }
+  addToList() {
+      if (this.newTask == '') {
+      }
+      else {
+          this.allItems.push(this.newTask);
+          this.newTask = '';
+      }
+      console.log(this.allItems)
+  }
+
+  // /* This function takes to input the
+  //    task, that has to be deleted*/
+   public deleteTask(index:any) {
+      this.allItems.splice(index, 1);
+   }
+}
